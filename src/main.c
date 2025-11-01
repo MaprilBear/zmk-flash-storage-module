3487d41efbd02c0d2c6b19d444a52c68b6de9fa8
April Douglas <maprilbear@gmail.com>
2025-11-01 14:11:15 -0700

    ZMK 0.4 Migration

--- src/main.c
+++ src/main.c
@@ -5,13 +5,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
-#include <zephyr/fs/fs.h>
+#include <sample_usbd.h>
+
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/usb/class/usbd_msc.h>
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
+#include <zephyr/usb/class/usbd_msc.h>
+#include <zephyr/fs/fs.h>
+#include <stdio.h>
 
 LOG_MODULE_REGISTER(main);
 
@@ -28,174 +30,140 @@ LOG_MODULE_REGISTER(main);
 FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
 #endif
 
-#define STORAGE_PARTITION storage_partition
-#define STORAGE_PARTITION_ID FIXED_PARTITION_ID(STORAGE_PARTITION)
+#if !defined(CONFIG_DISK_DRIVER_FLASH) && \
+	!defined(CONFIG_DISK_DRIVER_RAM) && \
+	!defined(CONFIG_DISK_DRIVER_SDMMC)
+#error No supported disk driver enabled
+#endif
+
+#define STORAGE_PARTITION		storage_partition
+#define STORAGE_PARTITION_ID		FIXED_PARTITION_ID(STORAGE_PARTITION)
 
 static struct fs_mount_t fs_mnt;
 
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
-USBD_CONFIGURATION_DEFINE(config_1, USB_SCD_SELF_POWERED, 200);
-
-USBD_DESC_LANG_DEFINE(sample_lang);
-USBD_DESC_MANUFACTURER_DEFINE(sample_mfr, "ZEPHYR");
-USBD_DESC_PRODUCT_DEFINE(sample_product, "Zephyr USBD MSC");
-USBD_DESC_SERIAL_NUMBER_DEFINE(sample_sn, "0123456789ABCDEF");
-
-USBD_DEVICE_DEFINE(sample_usbd, DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)),
-                   0x2fe3, 0x0008);
+static struct usbd_context *sample_usbd;
 
 #if CONFIG_DISK_DRIVER_RAM
-USBD_DEFINE_MSC_LUN(RAM, "Zephyr", "RAMDisk", "0.00");
+USBD_DEFINE_MSC_LUN(ram, "RAM", "Zephyr", "RAMDisk", "0.00");
 #endif
 
 #if CONFIG_DISK_DRIVER_FLASH
-USBD_DEFINE_MSC_LUN(NAND, "Zephyr", "FlashDisk", "0.00");
+USBD_DEFINE_MSC_LUN(nand, "NAND", "Zephyr", "FlashDisk", "0.00");
 #endif
 
 #if CONFIG_DISK_DRIVER_SDMMC
-USBD_DEFINE_MSC_LUN(SD, "Zephyr", "SD", "0.00");
+USBD_DEFINE_MSC_LUN(sd, "SD", "Zephyr", "SD", "0.00");
 #endif
 
-static int enable_usb_device_next(void) {
-  int err;
+static int enable_usb_device_next(void)
+{
+	int err;
 
-  err = usbd_add_descriptor(&sample_usbd, &sample_lang);
-  if (err) {
-    LOG_ERR("Failed to initialize language descriptor (%d)", err);
-    return err;
-  }
+	sample_usbd = sample_usbd_init_device(NULL);
+	if (sample_usbd == NULL) {
+		LOG_ERR("Failed to initialize USB device");
+		return -ENODEV;
+	}
 
-  err = usbd_add_descriptor(&sample_usbd, &sample_mfr);
-  if (err) {
-    LOG_ERR("Failed to initialize manufacturer descriptor (%d)", err);
-    return err;
-  }
+	err = usbd_enable(sample_usbd);
+	if (err) {
+		LOG_ERR("Failed to enable device support");
+		return err;
+	}
 
-  err = usbd_add_descriptor(&sample_usbd, &sample_product);
-  if (err) {
-    LOG_ERR("Failed to initialize product descriptor (%d)", err);
-    return err;
-  }
+	LOG_DBG("USB device support enabled");
 
-  err = usbd_add_descriptor(&sample_usbd, &sample_sn);
-  if (err) {
-    LOG_ERR("Failed to initialize SN descriptor (%d)", err);
-    return err;
-  }
-
-  err = usbd_add_configuration(&sample_usbd, &config_1);
-  if (err) {
-    LOG_ERR("Failed to add configuration (%d)", err);
-    return err;
-  }
-
-  err = usbd_register_class(&sample_usbd, "msc_0", 1);
-  if (err) {
-    LOG_ERR("Failed to register MSC class (%d)", err);
-    return err;
-  }
-
-  err = usbd_init(&sample_usbd);
-  if (err) {
-    LOG_ERR("Failed to initialize device support");
-    return err;
-  }
-
-  err = usbd_enable(&sample_usbd);
-  if (err) {
-    LOG_ERR("Failed to enable device support");
-    return err;
-  }
-
-  LOG_DBG("USB device support enabled");
-
-  return 0;
+	return 0;
 }
-#endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK_NEXT) */
+#endif /* defined(CONFIG_USB_DEVICE_STACK_NEXT) */
 
-static int setup_flash(struct fs_mount_t *mnt) {
-  int rc = 0;
+static int setup_flash(struct fs_mount_t *mnt)
+{
+	int rc = 0;
 #if CONFIG_DISK_DRIVER_FLASH
-  unsigned int id;
-  const struct flash_area *pfa;
+	unsigned int id;
+	const struct flash_area *pfa;
 
-  mnt->storage_dev = (void *)STORAGE_PARTITION_ID;
-  id = STORAGE_PARTITION_ID;
+	mnt->storage_dev = (void *)STORAGE_PARTITION_ID;
+	id = STORAGE_PARTITION_ID;
 
-  rc = flash_area_open(id, &pfa);
-  printk("Area %u at 0x%x on %s for %u bytes\n", id, (unsigned int)pfa->fa_off,
-         pfa->fa_dev->name, (unsigned int)pfa->fa_size);
+	rc = flash_area_open(id, &pfa);
+	printk("Area %u at 0x%x on %s for %u bytes\n",
+	       id, (unsigned int)pfa->fa_off, pfa->fa_dev->name,
+	       (unsigned int)pfa->fa_size);
 
-  if (rc < 0 && IS_ENABLED(CONFIG_APP_WIPE_STORAGE)) {
-    printk("Erasing flash area ... ");
-    rc = flash_area_erase(pfa, 0, pfa->fa_size);
-    printk("%d\n", rc);
-  }
+	if (rc < 0 && IS_ENABLED(CONFIG_APP_WIPE_STORAGE)) {
+		printk("Erasing flash area ... ");
+		rc = flash_area_flatten(pfa, 0, pfa->fa_size);
+		printk("%d\n", rc);
+	}
 
-  if (rc < 0) {
-    flash_area_close(pfa);
-  }
+	if (rc < 0) {
+		flash_area_close(pfa);
+	}
 #endif
-  return rc;
+	return rc;
 }
 
-static int mount_app_fs(struct fs_mount_t *mnt) {
-  int rc;
+static int mount_app_fs(struct fs_mount_t *mnt)
+{
+	int rc;
 
 #if CONFIG_FAT_FILESYSTEM_ELM
-  static FATFS fat_fs;
+	static FATFS fat_fs;
 
-  mnt->type = FS_FATFS;
-  mnt->fs_data = &fat_fs;
-  if (IS_ENABLED(CONFIG_DISK_DRIVER_RAM)) {
-    mnt->mnt_point = "/RAM:";
-  } else if (IS_ENABLED(CONFIG_DISK_DRIVER_SDMMC)) {
-    mnt->mnt_point = "/SD:";
-  } else {
-    mnt->mnt_point = "/E:";
-  }
+	mnt->type = FS_FATFS;
+	mnt->fs_data = &fat_fs;
+	if (IS_ENABLED(CONFIG_DISK_DRIVER_RAM)) {
+		mnt->mnt_point = "/RAM:";
+	} else if (IS_ENABLED(CONFIG_DISK_DRIVER_SDMMC)) {
+		mnt->mnt_point = "/SD:";
+	} else {
+		mnt->mnt_point = "/NAND:";
+	}
+
+  LOG_PRINTK("Mount point: %s\n", mnt->mnt_point);
 
 #elif CONFIG_FILE_SYSTEM_LITTLEFS
-  mnt->type = FS_LITTLEFS;
-  mnt->mnt_point = "/lfs";
-  mnt->fs_data = &storage;
+	mnt->type = FS_LITTLEFS;
+	mnt->mnt_point = "/lfs";
+	mnt->fs_data = &storage;
 #endif
+	rc = fs_mount(mnt);
 
-  mnt->mnt_point = "/NAND:";
-
-  rc = fs_mount(mnt);
-
-  return rc;
+	return rc;
 }
 
-static int setup_disk(void) {
-  struct fs_mount_t *mp = &fs_mnt;
-  struct fs_dir_t dir;
-  int rc;
+static void setup_disk(void)
+{
+	struct fs_mount_t *mp = &fs_mnt;
+	struct fs_dir_t dir;
+	int rc;
 
-  fs_dir_t_init(&dir);
+	fs_dir_t_init(&dir);
 
-  if (IS_ENABLED(CONFIG_DISK_DRIVER_FLASH)) {
-    rc = setup_flash(mp);
-    if (rc < 0) {
-      LOG_ERR("Failed to setup flash area");
-      return rc;
-    }
-  }
+	if (IS_ENABLED(CONFIG_DISK_DRIVER_FLASH)) {
+		rc = setup_flash(mp);
+		if (rc < 0) {
+			LOG_ERR("Failed to setup flash area");
+			return;
+		}
+	}
 
-  if (!IS_ENABLED(CONFIG_FILE_SYSTEM_LITTLEFS) &&
-      !IS_ENABLED(CONFIG_FAT_FILESYSTEM_ELM)) {
-    LOG_INF("No file system selected");
-    return -1;
-  }
+	if (!IS_ENABLED(CONFIG_FILE_SYSTEM_LITTLEFS) &&
+	    !IS_ENABLED(CONFIG_FAT_FILESYSTEM_ELM)) {
+		LOG_INF("No file system selected");
+		return;
+	}
 
-  rc = mount_app_fs(mp);
-  if (rc < 0) {
-    LOG_ERR("Failed to mount filesystem");
-    return rc;
-  }
+	rc = mount_app_fs(mp);
+	if (rc < 0) {
+		LOG_ERR("Failed to mount filesystem");
+		return;
+	}
 
-  return 0;
+	return;
 }
 
 SYS_INIT(setup_disk, APPLICATION, 99);
